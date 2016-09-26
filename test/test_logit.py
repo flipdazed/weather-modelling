657ffb2c3d33599c045027c30e98ce644b45c397
Alex McFarlane <alexander.mcfarlane@physics.org>
2016-09-26 23:07:31 +0100

    removed common elements from test_logit.py

--- test/test_logit.py
+++ test/test_logit.py
@@ -12,31 +12,6 @@ from mlp import *
 
 MODEL = os.path.join(load.model_dir, os.path.splitext(os.path.basename(__file__))[0]+'.pkl')
 
-def predict():
-    """
-    An example of how to load a trained model and use it
-    to predict labels.
-    """
-    
-    # load the saved model
-    classifier = pickle.load(open(MODEL))
-    
-    # compile a predictor function
-    predict_model = theano.function(
-        inputs=[classifier.input],
-        outputs=classifier.y_pred)
-    
-    # We can test it on some examples from test test
-    datasets = source.mnist()
-    test_set_x, test_set_y = datasets[2]
-    test_set_x = test_set_x.get_value()
-    
-    predicted_values = predict_model(test_set_x[:10])
-    logger.info("Predicted vs. Actual values for the first 10 examples in test set:")
-    logger.info(predicted_values)
-    logger.info(test_set_y[:10].eval())
-    pass
-#
 if __name__ == "__main__":
     logger = utils.logs.get_logger(__name__, update_stream_level=utils.logs.logging.DEBUG)
     logger.info('Loading data ...')
